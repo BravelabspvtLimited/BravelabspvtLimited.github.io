3e89bdafbb9c9799591cb6349407b48a8f417a59
abravelab <39159401+BravelabspvtLimited@users.noreply.github.com>
2018-06-18 18:57:37 +0530

    medgenome bugs fixed

--- medgenome-project/js/main.js
+++ medgenome-project/js/main.js
@@ -21,10 +21,6 @@
                 skillItems[x].style.display ='inline-block';
               }
 
-              // else if(this.textContent == 'All'){
-              //   skillItems[x].style.display = 'inline-block';
-              // }
-
               else{
                 skillItems[x].style.display ='none';
               }
@@ -39,7 +35,6 @@
 
 $(document).ready(function() {
 
-
 	$('.hamburger-btn').click(function(){
 		$('.site-nav').toggleClass('d_block');
 	});
@@ -50,20 +45,20 @@ $(document).ready(function() {
 
 	const $tabBtn = $('.mdgo-tab-btn');
 	const $tabContent = $('.mdgo-tab-content-wrap');
-	const $tabArrow = $('.tab-arrow');
+	// const $tabArrow = $('.tab-arrow');
 
 		$tabBtn.click(function(){
 			let $this = $(this);
 		
 			$tabContent.slideUp('slow');
-			$tabArrow.css({'transform': 'rotate(0deg)'});
+			// $tabArrow.css({'transform': 'rotate(0deg)'});
 
 			if($this.next().is(':visible')){
 				$this.next().slideUp('slow');
-				$this.children('.tab-arrow').css({'transform': 'rotate(0deg)'});
+				// $this.children('.tab-arrow').css({'transform': 'rotate(0deg)'});
 			}else{
 				$this.next().slideDown('slow');
-				$this.children('.tab-arrow').css({'transform': 'rotate(180deg)'});
+				// $this.children('.tab-arrow').css({'transform': 'rotate(180deg)'});
 			}
 			
 		});
@@ -243,12 +238,69 @@ $(document).ready(function() {
     });
 
 
-
   /*=====  End of Script For Select Option   ====*/
 
 
 
+  (function(){
 
+    $('.whole-genome-trans-tcr-nav ul li a').click(function(event){
+
+        if($(this).attr('href') == '#'){
+          event.preventDefault();
+        }
+
+    });
+
+  })();
+
+
+  (function($) {
+
+    $.fn.drags = function(opt) {
+
+        opt = $.extend({handle:"",cursor:"move"}, opt);
+
+        if(opt.handle === "") {
+            var $el = this;
+        } else {
+            var $el = this.find(opt.handle);
+        }
+
+        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+            if(opt.handle === "") {
+                var $drag = $(this).addClass('draggable');
+            } else {
+                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+            }
+            var z_idx = $drag.css('z-index'),
+                drg_h = $drag.outerHeight(),
+                drg_w = $drag.outerWidth(),
+                pos_y = $drag.offset().top + drg_h - e.pageY,
+                pos_x = $drag.offset().left + drg_w - e.pageX;
+            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+                $('.draggable').offset({
+                    top:e.pageY + pos_y - drg_h,
+                    left:e.pageX + pos_x - drg_w
+                }).on("mouseup", function() {
+                    $(this).removeClass('draggable').css('z-index', z_idx);
+                });
+            });
+            e.preventDefault(); // disable selection
+        }).on("mouseup", function() {
+            if(opt.handle === "") {
+                $(this).removeClass('draggable');
+            } else {
+                $(this).removeClass('active-handle').parent().removeClass('draggable');
+            }
+        });
+
+    }
+
+  })(jQuery);
+
+  $('.lightbox-img').drags();
 
 
 });
+
